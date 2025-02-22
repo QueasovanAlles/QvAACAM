4cc11919e032204d017fccd81b6ebdfa0b833d26
QueasovanAlles <your-email@example.com>
2025-02-22 08:58:17 +0100

    First full layout setup, Log and View QVAACAM functionality

--- app/src/main/java/com/qva/qvaacam/AndroidInterface.kt
+++ app/src/main/java/com/qva/qvaacam/AndroidInterface.kt
@@ -1,13 +1,29 @@
 package com.qva.qvaacam
 
+import android.content.pm.ActivityInfo
 import android.util.Log
 import android.webkit.JavascriptInterface
+import android.webkit.WebView
 
-class AndroidInterface(private val networkDiscovery: NetworkDiscovery) {
+class AndroidInterface(private val activity: MainActivity,
+                       private val webView: WebView,
+                       private val networkDiscovery: NetworkDiscovery) {
     @JavascriptInterface
     fun getMasterIP(): String {
         val status = networkDiscovery.getCurrentStatus()
         Log.d("AndroidInterface", "Returning status: $status")
         return "{\"ip\": \"${status.ip}\", \"status\": \"${status.status}\"}"
+    }
+    @JavascriptInterface
+    fun flipOrientation() {
+        activity.runOnUiThread {
+            val newOrientation = if (activity.requestedOrientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+                ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+            } else {
+                ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+            }
+            activity.requestedOrientation = newOrientation
+            webView.loadUrl(webView.url ?: "")
+        }
     }
 }
--- app/src/main/java/com/qva/qvaacam/MainActivity.kt
+++ app/src/main/java/com/qva/qvaacam/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.qva.qvaacam
 
+import android.content.pm.ActivityInfo
 import android.content.pm.PackageManager
+import android.content.res.Configuration
 import android.os.Bundle
 import android.util.Log
 import android.view.View
@@ -24,6 +26,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Lock rotation based on initial orientation
+        val currentOrientation = resources.configuration.orientation
+        val lockMode = if (currentOrientation == Configuration.ORIENTATION_LANDSCAPE) {
+            ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        } else {
+            ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        requestedOrientation = lockMode
+
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         setupEdgeToEdge()
@@ -56,11 +68,12 @@ class MainActivity : AppCompatActivity() {
             Log.d("NetworkScan", "Found host: $host")
         }
         webView.addJavascriptInterface(BatteryInterface(), "BatteryMonitor")
-        webView.addJavascriptInterface(AndroidInterface(networkDiscovery), "Android")
+        webView.addJavascriptInterface(AndroidInterface(this, webView, networkDiscovery), "Android")
     }
 
     private fun initializeServices() {
-        requestAudioPermissions()
+        if (!checkCameraAndMicPermissions())
+            requestCameraAndMicPermissions()
         batteryManager.monitorBattery { percentage, isCharging ->
             runOnUiThread {
                 webView.evaluateJavascript(
@@ -123,130 +136,24 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    /*
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-
-        webView = WebView(this)
-        setContentView(webView)
-
-        webView.setLayerType(View.LAYER_TYPE_HARDWARE, null)
-        webView.settings.apply {
-            javaScriptEnabled = true
-            allowFileAccess = true
-            //domStorageEnabled = true
-            //allowContentAccess = true
-            //setMixedContentMode(WebSettings.MIXED_CONTENT_ALWAYS_ALLOW)
-            //defaultTextEncodingName = "utf-8"
-            //setMediaPlaybackRequiresUserGesture(false)
-            //setDomStorageEnabled(true)
-        }
-
-        webView.webViewClient = object : WebViewClient() {
-            override fun shouldInterceptRequest(view: WebView, request: WebResourceRequest): WebResourceResponse? {
-                val uri = request.url
-                if (uri.scheme == "file") {
-                    if (uri.path?.endsWith(".js") == true) {
-                        try {
-                            val lastSegment = uri.lastPathSegment!!
-                            val input = assets.open("www/${lastSegment}")
-                            return WebResourceResponse("application/javascript", "UTF-8", input)
-                        } catch (e: Exception) {
-                            Log.d("WebView", "Error loading: ${uri}, ${e.message}")
-                        }
-                    }
-                    if (uri.path?.endsWith(".png") == true) {
-                        try {
-                            val input = assets.open("www/${uri.path!!.removePrefix("/")}")
-                            return WebResourceResponse("image/png", "UTF-8", input)
-                        } catch (e: Exception) {
-                            Log.d("WebView", "Error loading image: ${uri}")
-                        }
-                    }
-                }
-                return super.shouldInterceptRequest(view, request)
-            }
-            override fun onPageFinished(view: WebView?, url: String?) {
-                webView.evaluateJavascript("window.ready") { result ->
-                    if (result == "true") {
-                        val networkDiscovery = NetworkDiscovery(this@MainActivity)
-                        networkDiscovery.scanIPRange(this@MainActivity, 52230) { host ->
-                            Log.d("NetworkScan", "Found host: $host")
-                            webView.evaluateJavascript(
-                                "javascript:onMasterFound('$host', '52230')",
-                                null
-                            )
-                        }
-                    }
-                }
-            }
-        }
-
-        // WebChromeClient for permissions
-        webView.webChromeClient = object : WebChromeClient() {
-            override fun onPermissionRequest(request: android.webkit.PermissionRequest) {
-                request.grant(request.resources)
-            }
-        }
-
-        WebView.setWebContentsDebuggingEnabled(true)
-
-        requestAudioPermissions()
-        Log.d("Permissions", "Microphone permission: ${checkMicrophonePermission()}")
-
-        webView.addJavascriptInterface(BatteryInterface(), "BatteryMonitor")
-
-        webView.loadUrl("file:///android_asset/www/index.html")
-
-        batteryManager.monitorBattery { percentage, isCharging ->
-            runOnUiThread {
-                webView.evaluateJavascript(
-                    "window.dispatchEvent(new CustomEvent('batteryUpdate', { detail: ${BatteryInterface().getBatteryStatus(percentage, isCharging)} }))",
-                    null
-                )
-            }
-        }
-
-        webSocketManager.connect("ws://192.168.1.2:52230")
-
-        val networkDiscovery = NetworkDiscovery(this)
-        webView.addJavascriptInterface(AndroidInterface(networkDiscovery), "Android")
-
-        // Start scanning ips in LAN
-        networkDiscovery.scanIPRange(this, 52230) { host ->
-            // This will be called each time a host is found
-            Log.d("NetworkScan", "Found host: $host")
-            // Notify JavaScript when a host is found
-            webView.evaluateJavascript(
-                "javascript:onMasterFound('$host', '52230')",  // Added port parameter
-                null
-            )
-        }
-    }
-    */
     override fun onDestroy() {
         super.onDestroy()
         webSocketManager.disconnect()
     }
 
-    private fun checkMicrophonePermission(): Boolean {
-        return checkSelfPermission(android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
+    private fun checkCameraAndMicPermissions(): Boolean {
+        return checkSelfPermission(android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED &&
+                checkSelfPermission(android.Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED
     }
 
-    private fun requestAudioPermissions() {
-        if (checkSelfPermission(android.Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(
-                arrayOf(android.Manifest.permission.RECORD_AUDIO),
-                123
-            )
-        }
+    private fun requestCameraAndMicPermissions() {
+        requestPermissions(
+            arrayOf(
+                android.Manifest.permission.CAMERA,
+                android.Manifest.permission.RECORD_AUDIO
+            ),
+            123
+        )
     }
 
 }
